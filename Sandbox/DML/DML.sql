9d8f68e91ff69d6b63cc70af6bac78ebc1f97b50
risingcurve <gksdnjswhd10@naver.com>
2022-10-17 02:34:41 +0900

    gg

--- Sandbox/DML/DML.sql
+++ Sandbox/DML/DML.sql
@@ -27,5 +27,74 @@ SELECT country FROM users;
 
 SELECT DISTINCT country FROM users;
 
+SELECT DISTINCT country FROM users ORDER BY country;
+
+SELECT DISTINCT first_name, country FROM users;
+
+SELECT DISTINCT first_name, country FROM users ORDER BY country ASC;
+
+SELECT first_name, age, balance FROM users WHERE age >= 30;
+
+SELECT first_name, age, balance FROM users WHERE age >= 30 AND balance > 500000;
+
+SELECT first_name, last_name FROM users WHERE first_name LIKE '%호%';
+
+SELECT first_name, last_name FROM users WHERE first_name LIKE '%준';
+
+SELECT first_name, last_name, phone FROM users WHERE phone LIKE '02-%';
+
+SELECT first_name, last_name, age FROM users WHERE age LIKE '2_';
+
+SELECT first_name, last_name, phone FROM users WHERE phone LIKE '%-51__-%';
+
+SELECT first_name, country FROM users WHERE country in ('경기도', '강원도');
+
+SELECT first_name, country FROM users WHERE country = '경기도' or country = '강원도';
+
+SELECT first_name, country FROM users WHERE country NOT IN ('경기도', '강원도');
+
+SELECT first_name, age FROM users WHERE age BETWEEN 20 AND 30;
+
+SELECT first_name, age FROM users WHERE age >= 20 and age <= 30;
+
+SELECT first_name, age FROM users WHERE age NOT BETWEEN 20 and 30;
+
+SELECT first_name, age FROM users WHERE age < 20 OR age > 30;
+
+SELECT rowid, first_name FROM users LIMIT 10;
+
+SELECT first_name, balance FROM users ORDER BY balance DESC LIMIT 10;
+
+SELECT first_name, age FROM users ORDER BY age LIMIT 5;
+
+SELECT rowid, first_name FROM users LIMIT 10 OFFSET 10;
+
+SELECT count(*) FROM users;
+
+SELECT avg(age) FROM users WHERE age >= 30;
+
+SELECT country, count(*) FROM users GROUP BY country;
 
 
+
+
+create table classmates (
+    name text not null,
+    age integer not null,
+    address text not null
+);
+
+DROP TABLE classmate;
+
+INSERT INTO classmates (name, age, address)
+VALUES ('홍길동', 23, '서울');
+
+INSERT INTO classmates
+VALUES
+    ('김철수', 30, '경기'),
+    ('이영미', 31, '강원');
+
+UPDATE classmates
+SET name='김철수한무두루미',
+    address='제주도'
+WHERE rowid = 2;
